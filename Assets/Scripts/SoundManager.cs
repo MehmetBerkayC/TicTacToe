634300b89522f9bb63d405eaff76fe3a7ab89c96
Mehmet Berkay Coruk <beqostore@gmail.com>
2025-01-14 18:07:12 +0300

    Sound complete

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,10 +3,28 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private Transform placeSFXPrefab;
+    [SerializeField] private Transform winSFXPrefab;
+    [SerializeField] private Transform loseSFXPrefab;
 
     private void Start()
     {
         GameManager.Instance.OnObjectPlaced += GameManager_OnObjectPlaced;
+        GameManager.Instance.OnGameWin += GameManager_OnGameWin; ;
+
+    }
+
+    private void GameManager_OnGameWin(object sender, GameManager.OnGameWinEventArgs e)
+    {
+        if (GameManager.Instance.GetLocalPlayerType() == e.winnerPlayerType)
+        {
+            Transform sfxTransform = Instantiate(winSFXPrefab);
+            Destroy(sfxTransform.gameObject, 5f);
+        }
+        else
+        {
+            Transform sfxTransform = Instantiate(loseSFXPrefab);
+            Destroy(sfxTransform.gameObject, 5f);
+        }
     }
 
     private void GameManager_OnObjectPlaced(object sender, System.EventArgs e)
